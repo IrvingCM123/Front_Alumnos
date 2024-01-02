5ecb78f52afba57bbdeb3566622942c656bc1076
Irving R. Conde Marín <98989957+IrvingCM123@users.noreply.github.com>
2024-01-02 14:52:06 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  endpoint: 'https://alumnoback-service-irvingcm123.cloud.okteto.net/api/v1/estudiantes',
+  firebase: {
+    apiKey: 'AIzaSyDIahzHtIBUdWUOWGam_MesAxCUBgkcQkg',
+    authDomain: 'sistemaasistencia-9d5e1.firebaseapp.com',
+    projectId: 'sistemaasistencia-9d5e1',
+    storageBucket: 'sistemaasistencia-9d5e1.appspot.com',
+    messagingSenderId: '245812703918',
+    appId: '1:245812703918:web:f7bb02db4657ebfe5c333f',
+    measurementId: 'G-TW86Z81QP6',
+  }
 };
